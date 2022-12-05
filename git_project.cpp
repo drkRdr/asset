df56451ee527fcaf93d78360b89f6061c8636ed7
Sedat Ulas <sedatulas_ehm@hotmail.com>
2022-12-06 01:12:29 +0300

    -Master

---
+++ git_project.cpp
@@ -0,0 +1,11 @@
+// git_project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Master\n";
+}
+
+
